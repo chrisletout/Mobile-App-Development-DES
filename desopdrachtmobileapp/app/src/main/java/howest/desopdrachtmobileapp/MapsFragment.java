9250df9bccc31f1f797de490ebd7a6b9bdaa797a
christophe Kurzawa <christophe.kurzawa@student.howest.be>
2015-03-30 11:16:07 +0200

    -- init Des opdracht -- map tonen

---
+++ desopdrachtmobileapp/app/src/main/java/howest/desopdrachtmobileapp/MapsFragment.java
@@ -0,0 +1,24 @@
+package howest.desopdrachtmobileapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chris on 30/03/15.
+ */
+public class MapsFragment extends android.support.v4.app.Fragment {
+    View rootView;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_maps, container, false);
+        return rootView;
+    }
+}
