4406f720317ff74d37ea606514a6f6b5b854ca8f
christophe Kurzawa <christophe.kurzawa@student.howest.be>
2015-04-02 10:14:39 +0200

    -- Fixed google map -- Showing current Location

--- desopdrachtmobileapp/app/src/main/java/howest/desopdrachtmobileapp/MapsFragment.java
+++ desopdrachtmobileapp/app/src/main/java/howest/desopdrachtmobileapp/MapsFragment.java
@@ -1,19 +1,32 @@
 package howest.desopdrachtmobileapp;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tagmanager.Container;
 
 /**
  * Created by chris on 30/03/15.
  */
 public class MapsFragment extends android.support.v4.app.Fragment {
     View rootView;
-
+    private GoogleMap map;
+    private SupportMapFragment fragment;
 
 
     @Nullable
@@ -21,21 +34,62 @@ public class MapsFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        rootView = inflater.inflate(R.layout.fragment_maps, container, false);
 //        return rootView;
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+//        SupportMapFragment mMapFragment = SupportMapFragment.newInstance();
+//        mMap = mMapFragment.getMap();
+//
+//        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+//        transaction.add(R.id.map_container, mMapFragment).commit();
+//        return view;
 
+//        try {
+//            rootView = inflater.inflate(R.layout.fragment_maps, container, false);
+//
+//
+//        } catch (InflateException e) {
+//        /* map is already there, just return view as it is */
+//        }
+//        return rootView;
 
+    }
+    public void onMapReady(GoogleMap map) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(-18.142, 178.431), 2));
 
-        try {
-            rootView = inflater.inflate(R.layout.fragment_maps, container, false);
-        } catch (InflateException e) {
-        /* map is already there, just return view as it is */
-        }
-        return rootView;
+        // Other supported types include: MAP_TYPE_NORMAL,
+        // MAP_TYPE_TERRAIN, MAP_TYPE_HYBRID and MAP_TYPE_NONE
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
 
     }
 
     @Override
-    public void onDestroy() {
-//        fragmentTransaction.remove(yourfragment).commit();
-        super.onDestroy();
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (map == null) {
+            map = fragment.getMap();
+//            map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+            map.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentManager fm = getChildFragmentManager();
+        fragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+        if (fragment == null) {
+            fragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, fragment).commit();
+        }
     }
 }
